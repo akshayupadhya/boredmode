150bd261761aea2b6e734b9b8fd95f1394b7665b
akshay upadhya <akshaysrinagar@gmail.com>
2018-11-24 23:03:56 +0530

    cluster module main.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import express from 'express'
 
 const app = express()
 
-app.listen('3000', () => {
-	console.log(`app listening on port 3000`)
+app.get('/',(req,res) => {
+	res.send(`hello there`)
 })
+
+// app.listen('3000', () => {
+// 	console.log(`app listening on port 3000`)
+// })
+
+export default app
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import express, { Application } from 'express'
+import app from './app'
+import { isMaster, fork } from 'cluster'
+import { config as env } from 'dotenv'
+env()
+function createServer () {
+	const server: Application = express()
+	server.use(app)
+	const port: Number | string = process.env.PORT || 3000
+	server.listen(port, () => {
+		console.log(`listening on port ${port}`)
+	})
+}
+
+// Code to run if we're in the master process
+if (isMaster && process.env.NODE_ENV !== 'development') {
+	// Count the machine's CPUs
+	const cpuCount = require('os').cpus().length
+
+	// Create a worker for each CPU
+	for (let i = 0; i < cpuCount; i += 1) {
+		fork()
+	}
+	// Code to run if we're in a worker process
+} else {
+	createServer()
+}
