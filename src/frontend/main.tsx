fe3309f29bf1c9d69584b5dfaa92813dd3cbfef9
akshay upadhya <akshaysrinagar@gmail.com>
2018-11-26 11:15:57 +0530

    hello world component

---
+++ src/frontend/main.tsx
@@ -0,0 +1,10 @@
+import React,{ Component } from 'react'
+export default class Main extends Component {
+	render () {
+		return (
+			<h1 className='hello'>
+				hello from server react
+			</h1>
+		)
+	}
+}
