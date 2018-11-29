d0a04f3363f0ce972f502dfb2058aa864e1146e7
akshay upadhya <akshaysrinagar@gmail.com>
2018-11-29 10:53:45 +0530

    dropping console logs

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,11 +1,10 @@
-import React,{ Component  } from 'react'
+import React,{ Component } from 'react'
 interface Props {
 	data: string
 }
 export default class AppComponent extends Component<Props,{}> {
 	constructor (props: Props) {
 		super(props)
-		console.log(props,'props')
 	}
 	public render () {
 		return (
