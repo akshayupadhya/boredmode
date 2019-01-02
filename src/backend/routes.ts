9a428e2ad1b9475814a3e81e0246c34081ee58b1
akshay upadhya <akshaysrinagar@gmail.com>
2019-01-02 16:35:16 +0530

    express graphql setup

--- src/backend/routes.ts
+++ src/backend/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import indexController from './controllers/index'
+import { messagesController } from './graphql/gql'
 
 const api: Router = Router()
 
 api.get('/', indexController)
+api.use('/messages',messagesController)
 
 export default api
